85a903a1c2b08fd6ab8de208471453cd4c0ea130
nyanchan <personal@nyanchangames.com>
2019-12-09 13:55:12 +0900

    Slack post handshake

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Slack からの webuHook をハンドシェイク
+app.post('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(req.body.challenge);
+});
+
 app.get("/", (request, response) => {
     response.status(200).send("Hello World");
 });
