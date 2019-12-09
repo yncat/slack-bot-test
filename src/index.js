7b2f04ea4d2f19489846a0668981bc968e42b297
nyanchan <personal@nyanchangames.com>
2019-12-09 15:30:04 +0900

    Receive test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-var express = require("express");
-var bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require('body-parser');
+const Slack = require('slack');
+const slack = new Slack();
 
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+/*
 
 //Slack からの webuHook をハンドシェイク
 app.post('/', (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.send(req.body.challenge);
+});
+*/
+
+app.post('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send("");
+slack.chat.postMessage({
+  token: process.env.SLACK_BOT_TOKEN,
+  channel: "CRETBBSV6",
+  text: "私は、びーだぶりゅーぼっとちゃんです。",
+  username: "BW-bot"
+},console.log);
 });
 
 app.get("/", (request, response) => {
